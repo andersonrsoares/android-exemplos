4948fc5ff42f4e73791629d3d2a56176e9143cb7
Anderson R Soares <anderson.r.soares@gmail.com>
2018-07-20 14:51:40 -0300

    teste 3-1

--- ExpressoExemplo/app/src/androidTest/java/br/com/andersonrsoares/expressoexemplo/LoginActivityTest.java
+++ ExpressoExemplo/app/src/androidTest/java/br/com/andersonrsoares/expressoexemplo/LoginActivityTest.java
@@ -1,26 +1,37 @@
 package br.com.andersonrsoares.expressoexemplo;
 
 
+
+
+
+
+
+import android.app.Activity;
+import android.app.Instrumentation.ActivityResult;
+import android.content.Intent;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
 import com.example.heitorcolangelo.espressotests.R;
 import com.example.heitorcolangelo.espressotests.ui.activity.LoginActivity;
-
+import com.example.heitorcolangelo.espressotests.ui.activity.MainActivity;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 
 @RunWith(AndroidJUnit4.class)
@@ -61,5 +72,21 @@ public class LoginActivityTest {
         onView(withId(R.id.login_button)).perform(click());
         onView(withText(R.string.validation_message)).check(matches(isDisplayed()));
         onView(withText(R.string.ok)).perform(click());
+    }
+
+    @Test
+    public void whenBothFieldsAreFilled_andClickOnLoginButton_shouldOpenMainActivity() {
+        Intents.init();
+        onView(withId(R.id.login_username)).perform(typeText("defaultText"), closeSoftKeyboard());
+        onView(withId(R.id.login_password)).perform(typeText("defaultText"), closeSoftKeyboard());
+        Matcher<Intent> matcher = hasComponent(MainActivity.class.getName());
+
+        ActivityResult result = new ActivityResult(Activity.RESULT_OK, null);
+
+        intending(matcher).respondWith(result);
+
+        onView(withId(R.id.login_button)).perform(click());
+        intended(matcher);
+        Intents.release();
     }
 }
---
+++ ExpressoExemplo/app/src/androidTest/java/com/example/heitorcolangelo/espressotests/ui/activity/Login2ActivityTest.java
@@ -0,0 +1,77 @@
+package com.example.heitorcolangelo.espressotests.ui.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.heitorcolangelo.espressotests.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class Login2ActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void login2ActivityTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.login_button), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("Ok"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonPanel),
+                                        0),
+                                3)));
+        appCompatButton2.perform(scrollTo(), click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
